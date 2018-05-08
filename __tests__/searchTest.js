a78e849c1563fc27b07de2249622210bf60694c9
tawashley <trisashley@gmail.com>
2018-05-08 12:31:21 +0100

    refactor: Test structuring

--- __tests__/searchTest.js
+++ __tests__/searchTest.js
@@ -1,39 +1,44 @@
 const puppeteer = require('puppeteer');
 
-test('Plumbs Search Journey', async () => {
+describe('Search journey test', async () => {
+    test('Plumbs Search Journey', async (done) => {
 
-    let browser = await puppeteer.launch({args: ['--no-sandbox']});
+        let browser = await puppeteer.launch({args: ['--no-sandbox'], headless: false});
 
-    let page = await browser.newPage();
+        let page = await browser.newPage();
 
-    await page.setViewport({
-        width: 1440,
-        height: 900
-    });
+        await page.setViewport({
+            width: 1440,
+            height: 900
+        });
 
-    await page.goto('https://www.plumbs.co.uk/', { waitUntil: 'domcontentloaded'});
-    await page.click('#search_toggle', { delay: 250 });
+        await page.goto('https://www.plumbs.co.uk/', { waitUntil: 'domcontentloaded'});
+        await page.click('#search_toggle', { delay: 250 });
 
-    const searchInput = await page.$('#search_drop_down');
-    await searchInput.type('covers', { delay: 250 })
-    await searchInput.press('Enter');
+        const searchInput = await page.$('#search_drop_down');
+        await searchInput.type('covers', { delay: 250 })
+        await searchInput.press('Enter');
 
-    await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-    var searchHeadingText = await page.$eval('h1.red', (element) => element.innerHTML.trim());
-    var searchPageTitle = await page.title();
+        var searchHeadingText = await page.$eval('h1.red', (element) => element.innerHTML.trim());
+        var searchPageTitle = await page.title();
 
-    expect(searchHeadingText).toBe('Search');
-    expect(searchPageTitle).toBe('Search | Plumbs');
+        expect(searchHeadingText).toBe('Search');
+        expect(searchPageTitle).toBe('Search | Plumbs');
 
-    await page.click('.search-new li a');
+        await page.click('.search-new li a');
 
-    await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-    await page.waitForSelector('.main_content_area', {
-        visible: true
-    });
+        await page.waitForSelector('.main_content_area', {
+            visible: true
+        });
 
-    browser.close();
+        browser.close();
 
-}, 30000)
+        done();
+    }, 30000)
+})
+
+
